a7bed0829ec960c5eae84237c58e6e7f337fd488
Vaddi Govardhan <71170706+govardhan666@users.noreply.github.com>
2024-04-10 14:44:08 +0530

    Update MainActivity.java

--- Weather_App/app/src/main/java/com/example/weather_app/MainActivity.java
+++ Weather_App/app/src/main/java/com/example/weather_app/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 String city = cityName.getText().toString();
                 try{
                     if(city!=null){
-                        url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d3e1a6d4468a8819de7625a952e08b1c";
+                        url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid={Enter your api Key Here}";
                     }else{
                         Toast.makeText(MainActivity.this, "Enter City", Toast.LENGTH_SHORT).show();
                     }
